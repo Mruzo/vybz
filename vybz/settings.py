c615bddf5cba1d0d4334b9bc8e2eb63bcc662cd2
chris <chrisuzoewulu@gmail.com>
2023-05-22 15:16:09 -0400

    fixed template location in settings file

--- vybz/settings.py
+++ vybz/settings.py
@@ -106,7 +106,7 @@ ROOT_URLCONF = 'vybz.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(PROJECT_DIR, "templates")],
+        'DIRS': [os.path.join(PROJECT_DIR, "vybz/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
