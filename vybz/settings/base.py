8695564f9657421353c7e2388e925d27f889b675
chris <chrisuzoewulu@gmail.com>
2023-07-22 19:24:15 -0400

    update settings

--- vybz/settings/base.py
+++ vybz/settings/base.py
@@ -143,18 +143,6 @@ AUTH_PASSWORD_VALIDATORS = [
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backend.postgresql',
-        'NAME': 'vybz',
-        'USER': 'misteruzo',
-        'PASSWORD': 'Ulu136226!',
-        'HOST': 'localhost',
-        'PORT': '',
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
